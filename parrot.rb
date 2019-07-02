9f790469ce565fd04994c15fe2bad01207e238ab
Bruce Ruffin <bruffin1221@gmail.com>
2019-07-02 15:30:11 +0000

    Automatically backed up by Learn

--- parrot.rb
+++ parrot.rb
@@ -1,2 +1,7 @@
-# Create method `parrot` that outputs a given phrase and
-# returns the phrase
+def parrot(sound="Squawk!")
+  phrase="Pretty bird!"
+  puts "#{sound}"
+  return phrase
+end
+
+parrot
